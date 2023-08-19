22df43dee4b80de5af5f15e82d1b14c572253911
oleh dlubovskyi <oleh.d@delasport.com>
2023-08-20 00:36:26 +0200

    initial

---
+++ src/main/java/io/genevjov/wikilistener/WikiListenerApplication.java
@@ -0,0 +1,13 @@
+package io.genevjov.wikilistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/genevjov/wikilistener/WikiListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.genevjov.wikilistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
